5bb66319cbd1d415444d75a3de4aba3876f475e1
root <root@DESKTOP-JC5S8UD>
2024-07-10 13:11:43 +0800

    first

---
+++ main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int (*p)[5] = NULL;//指向拥有5个整型数据的指针
+    int arr[5]={1,3,5,7,9};
+    p = &arr;//整个数组的首地址
+    printf("arr = %p\n",arr);
+    printf("arr +1 = %p\n",arr+1);
+    printf("&arr = %p\n",arr);
+    printf("&arr +1 = %p\n",&arr+1);
+    printf("p = %p\n",p);
+    printf("p +1 = %p\n",p+1);
+    printf("*p  = %p\n",*p);
+    printf("(*p) +1 = %p\n",(*p));
+    return 0;
+}
